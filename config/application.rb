73d9386ef62a0471531ebf42cffd142f3642a744
Julie Ahlquist <julieahlquist@hotmail.com>
2019-08-14 15:16:17 +0200

    configures rack-cors in application.rb to request get, post, put and delete from any resource

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative 'boot'
 
 require "rails"
-# Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
@@ -10,11 +9,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module GnosisApi
@@ -28,6 +23,12 @@ module GnosisApi
       generate.controller_specs false
       generate.routing_specs false
       generate.helper_specs false
+
+      config.middleware.insert_before 0, Rack::Cors do
+        allow do
+          origins '*'
+          resource '*', headers: :any, methods: [:get, :post, :options]
+        end
     end
   end
 end

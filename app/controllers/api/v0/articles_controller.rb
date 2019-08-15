df85fd43a5b42c1066a9c3a0b7ac750ab58ca969
oliverochman <oliver.ochman@hotmail.com>
2019-08-15 12:20:49 +0200

    render articles in json

--- app/controllers/api/v0/articles_controller.rb
+++ app/controllers/api/v0/articles_controller.rb
@@ -1,5 +1,6 @@
 class Api::V0::ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    articles = Article.all
+    render json: articles, each_serializer: Articles::IndexSerializer
   end
 end

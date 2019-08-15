91ab73204b148fa4609dfe642a789361b1f634f3
oliverochman <oliver.ochman@hotmail.com>
2019-08-15 12:20:20 +0200

    add spec helper method for json_response and refactor request spec

--- spec/requests/api/v0/articles_spec.rb
+++ spec/requests/api/v0/articles_spec.rb
@@ -1,22 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Api::V0::ArticlesController, type: :request do
-
+  let(:headers) { {HTTP_ACCEPT: "application/json"} }
 
   describe 'GET /v0/articles' do
     before do
       5.times { FactoryBot.create(:article) }
+      get '/api/v0/articles', headers: headers
+    end
+
+
+    it 'should return collection of articles' do    
       
       binding.pry
       
+      expect(json_response.count). to eq 5
     end
 
-    it 'should return Articles' do
-      get '/api/v0/articles'
-
-      json_response = JSON.parse(response.body)
+    it 'returns 200 response' do
       expect(response.status).to eq 200
-      expect(json_response['message']). to eq 'Articles'
     end
+
   end
 end
+

100f11fc5521f94471ead0485b1f49a658f440fe
Julie Ahlquist <julieahlquist@hotmail.com>
2019-08-15 19:49:46 +0200

    fixes error by extending devise in uni model

--- app/models/university.rb
+++ app/models/university.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class University < ActiveRecord::Base
+  extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,

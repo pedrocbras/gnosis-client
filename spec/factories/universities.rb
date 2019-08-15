71883022d4f6e4113297303954af3e758c1c61df
Julie Ahlquist <julieahlquist@hotmail.com>
2019-08-15 19:18:42 +0200

    creates factory bot for universities

---
+++ spec/factories/universities.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :university do
+    name { "MyString" }
+    location { "MyString" }
+    email { "MyString" }
+    password { "MyString" }
+    password_confirmation { "MyString" }
+  end
+end

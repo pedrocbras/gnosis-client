0e1a216eba9d35c4b7fa7f822e5d8a6e4e2096f9
Max Aubain <max.aubain@gmail.com>
2019-08-20 13:44:12 +0200

    Adds create article method with authentication for research group

--- app/controllers/api/v0/articles_controller.rb
+++ app/controllers/api/v0/articles_controller.rb
@@ -1,9 +1,20 @@
 class Api::V0::ArticlesController < ApplicationController
+  before_action :authenticate_api_v0_user!, only: [:create]
+  
   def index
     articles = Article.all
     render json: articles, each_serializer: Articles::IndexSerializer
   end
 
   def create
+    article = current_api_v0_user.articles.create(article_params)
+
+    binding.pry
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :author)
   end
 end

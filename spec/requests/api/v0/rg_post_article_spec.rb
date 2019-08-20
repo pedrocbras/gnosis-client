57f6b12cb50cd85ba43f725cfa3d78cb89b0c14d
pedro_bras <pedro.c.bras@gmail.com>
2019-08-20 13:02:02 +0200

    refactors request spec file including facory bot and credentials on the request block, adds first test expecting response status

--- spec/requests/api/v0/rg_post_article_spec.rb
+++ spec/requests/api/v0/rg_post_article_spec.rb
@@ -1,23 +1,25 @@
-include Warden::Test::Helpers
-
 RSpec.describe Api::V0::ArticlesController, type: :request do
-  let(:headers) { {HTTP_ACCEPT: "application/json"} }
+  let(:rg_user) { FactoryBot.create(:user, role: :research_group) }
+  let(:credentials) { rg_user.create_new_auth_token }
+  let(:headers) { {HTTP_ACCEPT: "application/json"}.merge!(credentials) }
 
   describe 'research group POST article' do 
-    before 'create research group user' do
-      rg_user = FactoryBot.create(:user, role: :research_group)
-      login_as(rg_user, scope: :user)
-      binding.pry
+    before do
+      post '/api/v0/articles' , params: {
+        article: {
+          title: 'Test article',
+          body: 'Lorum lorum lorum',
+          author: rg_user
+        }
+      }, headers: headers
     end
 
-    it 'research group user creates new article' do
-      #rg_article = Article.create(:article, title: 'title',
-      #                                      body: 'body',
-      #                                      author: rg_user.email)
+    it 'research group user creates new article' do  
+      binding.pry
+
+      expect(response.status).to eq 200
     end
 
   end
 
 end
-
-Warden.test_reset!

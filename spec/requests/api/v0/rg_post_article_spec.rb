18920c0d3ffef7bb27544f28678d6d69b1a7302a
Max Aubain <max.aubain@gmail.com>
2019-08-20 11:25:23 +0200

    Refactors test in rg_post_article_spec.rb

--- spec/requests/api/v0/rg_post_article_spec.rb
+++ spec/requests/api/v0/rg_post_article_spec.rb
@@ -1,18 +1,23 @@
+include Warden::Test::Helpers
+
 RSpec.describe Api::V0::ArticlesController, type: :request do
   let(:headers) { {HTTP_ACCEPT: "application/json"} }
 
   describe 'research group POST article' do 
     before 'create research group user' do
       rg_user = FactoryBot.create(:user, role: :research_group)
-      # need to have rg_user sign in = WARDEN gem
+      login_as(rg_user, scope: :user)
+      binding.pry
     end
 
-    it 'research group user creates new article'
-      rg_article = Article.create(:article, title: 'title',
-                                            body: 'body',
-                                            author: rg_user.email)
+    it 'research group user creates new article' do
+      #rg_article = Article.create(:article, title: 'title',
+      #                                      body: 'body',
+      #                                      author: rg_user.email)
     end
 
   end
 
 end
+
+Warden.test_reset!

f0558f0f27d4aebf4da9c9cbf11805ceaaabe431
Max Aubain <max.aubain@gmail.com>
2019-08-20 17:42:42 +0200

    Refactors articles_spec.rb to assign new articles to a pre-existing user

--- spec/requests/api/v0/articles_spec.rb
+++ spec/requests/api/v0/articles_spec.rb
@@ -1,15 +1,16 @@
 RSpec.describe Api::V0::ArticlesController, type: :request do
   let(:headers) { {HTTP_ACCEPT: "application/json"} }
+  let(:rg_user) { FactoryBot.create(:user, role: :research_group) }
 
   describe 'GET /v0/articles' do
     before do
-      1.times { FactoryBot.create(:article) }
+      5.times { FactoryBot.create(:article, user: rg_user) }
       get '/api/v0/articles', headers: headers
     end
 
 
     it 'should return collection of articles' do    
-      expect(response_json.count). to eq 1
+      expect(response_json.count). to eq 5
     end
 
     it 'returns 200 response' do

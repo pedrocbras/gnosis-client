91056b038cc20106c139bda9f2b72b64da2f4215
pedro_bras <pedro.c.bras@gmail.com>
2019-08-21 16:15:59 +0200

    fixes code formating

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,10 @@ require 'faker'
 user = FactoryBot.create(:user, role: :research_group)
 
 6.times do |n|
-	title = Faker::Lorem.sentences(number: 1)
-	body = Faker::Lorem.paragraph
+  title = Faker::Lorem.sentences(number: 1)
+  body = Faker::Lorem.paragraph
   author = Faker::Name.name
-  
-	Article.create!(
-		title: title,
-		body: body,
-    author: user,
-		created_at: '')
-		puts "created article #{n+1}"
+
+  Article.create!(title: title, body: body, author: user, created_at: '')
+  puts "created article #{n + 1}"
 end

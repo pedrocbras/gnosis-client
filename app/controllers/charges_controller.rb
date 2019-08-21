02c833e2037527551e5e7c5289c33e3aed83d9c6
Seth NeJame <sethnejame@gmail.com>
2019-08-21 22:13:43 +0200

    Adding charges controller to handle stripe payments

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,28 @@
+require 'stripe'
+
+class Api::V0::ChargesController < ApplicationController
+  def create
+    Stripe.api_key = 'insert test key here'
+
+    begin
+      customer =
+        Stripe::Customer.create(
+          email: current_user.email, source: params[:charge][:token]
+        )
+
+      charge =
+        Stripe::Charge.create(
+          {
+            customer: customer.id,
+            amount: params[:charge][:amount],
+            description: params[:charge][:description],
+            currency: params[:charge][:currency]
+          },
+          { idempotency_key: ip_key }
+        )
+    rescue Stripe::CardError => e
+      render json: { message: 'Something bad happened. . .' },
+             status: :not_acceptable
+    end
+  end
+end

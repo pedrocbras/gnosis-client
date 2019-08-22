19b99be4809d4fe50ac614208bb77b0ae09f2d82
Seth NeJame <sethnejame@gmail.com>
2019-08-22 08:10:32 +0200

    Modeling controller after Thomas' stripe vid

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,28 +1,17 @@
-require 'stripe'
-
 class Api::V0::SubscriptionsController < ApplicationController
   def create
-    Stripe.api_key = 'insert test key here'
+    binding.pry
+    customer =
+      Stripe::Customer.create(
+        email: params[:stripeEmail], source: params[:stripeToken]
+      )
 
-    begin
-      customer =
-        Stripe::Customer.create(
-          email: current_user.email, source: params[:charge][:token]
-        )
-
-      charge =
-        Stripe::Charge.create(
-          {
-            customer: customer.id,
-            amount: params[:charge][:amount],
-            description: params[:charge][:description],
-            currency: params[:charge][:currency]
-          },
-          { idempotency_key: ip_key }
-        )
-    rescue Stripe::CardError => e
-      render json: { message: 'Something bad happened. . .' },
-             status: :not_acceptable
-    end
+    charge =
+      Stripe::Charge.create(
+        customer: customer.id,
+        amount: 10_000,
+        description: 'Gnosis Yearly Subscription',
+        currency: 'SEK'
+      )
   end
 end
